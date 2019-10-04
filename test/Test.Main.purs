4b53fc5bbc725e003a4f308f22457c89b6a990c1
Brandon Barker <beb82@cornell.edu>
2019-10-04 20:57:35 +0000

    confirmed insert is working

--- test/Test.Main.purs
+++ test/Test.Main.purs
@@ -8,18 +8,37 @@ import Effect as Effect
 import Effect.Console (logShow)
 import Foreign (readString, unsafeToForeign)
 import Option as Opt
+import Option (Option(..))
 import Test.Assert (assert)
 import Test.Unit.Console (print)
 
-someOption :: Opt.Option ( foo :: Boolean, bar :: Int )
-someOption = Opt.empty
-bar1 :: Int
-bar1 = 31
-anotherOption :: Opt.Option ( foo :: Boolean, bar :: Int )
-anotherOption = Opt.set (SProxy :: _ "bar") bar1 someOption
-
 main :: Effect.Effect Unit
 main = do
+  insertTest1
+  setTest1
+
+
+insertTest1 :: Effect.Effect Unit
+insertTest1 = do
   logShow anotherOption
-  logShow $ show $ runExceptT $ readString $ unsafeToForeign anotherOption
   assert $ Just bar1 == (Opt.get (SProxy :: _ "bar") anotherOption)
+  where
+    someOption :: Opt.Option ( foo :: Boolean )
+    someOption = Opt.empty
+    bar1 :: Int
+    bar1 = 31
+    anotherOption :: Opt.Option ( foo :: Boolean, bar :: Int )
+    anotherOption = Opt.insert (SProxy :: _ "bar") bar1 someOption
+
+setTest1 :: Effect.Effect Unit
+setTest1 = do
+  logShow anotherOption
+  -- logShow $ show $ runExceptT $ readString $ unsafeToForeign anotherOption
+  assert $ Just bar1 == (Opt.get (SProxy :: _ "bar") anotherOption)
+  where
+    someOption :: Opt.Option ( foo :: Boolean, bar :: Int )
+    someOption = Opt.empty
+    bar1 :: Int
+    bar1 = 31
+    anotherOption :: Opt.Option ( foo :: Boolean, bar :: Int )
+    anotherOption = Opt.set (SProxy :: _ "bar") bar1 someOption
